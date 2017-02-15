fc53961253ecf9909a2946b95abd3b28808f5a4d
sutap <sutap@yahoo.com>
2017-02-15 14:45:20 +0530

    bootstrap second commit

---
+++ bootstrap/sutap.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <title>Bootstrap Example</title>
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<div class="row">
+    	<div id="left" class="col-md-4">
+    		<header id="header" style="background-position: left -40.25px;">
+				<div class="inner">
+					<a class="image avatar" href="#"><img alt="" src="avatar.jpg" style="border-radius:100%;"></a>
+					<h1><strong>I am Strata</strong>, a super simple<br>
+					responsive site <br>template freebie<br>
+					crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+				</div>
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+						<li><a class="icon fa-github" href="#"><span class="label">Github</span></a></li>
+						<li><a class="icon fa-dribbble" href="#"><span class="label">Dribbble</span></a></li>
+						<li><a class="icon fa-envelope-o" href="#"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li><li>Demo Images: <a href="http://unsplash.com">Unsplash</a></li>
+					</ul>
+				</div>
+			</footer>
+			</header>
+    	</div>
+    	 <div class="col-md-8" style="background-color:white;" >
+    	 			<section id="one">
+						<header class="major">
+							<h2>Ipsum lorem dolor aliquam ante commodo<br>
+							magna sed accumsan arcu neque.</h2>
+						</header>
+						<p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae <br>lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+						<ul class="actions">
+							<li><a class="button" href="#">Learn More</a></li>
+						</ul>
+					</section>
+					<section id="two">
+						<h2>Recent Work</h2>
+						<div class="row">
+							<article class="6u 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Magna sed consequat tempus</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Ultricies lacinia interdum</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Tortor metus commodo</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Quam neque phasellus</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Nunc enim commodo aliquet</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img alt="" src="images/1235.jpeg"></a>
+								<h3>Risus ornare lacinia</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+						</div>
+						<ul class="actions">
+							<li><a class="button" href="#">Full Portfolio</a></li>
+						</ul>
+					</section>
+
+			<section id="three">
+						<h2>Get In Touch</h2>
+						<p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+						<div class="row">
+							<div class="8u 12u$(small)">
+								<form action="#" method="post">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)"><input type="text" placeholder="Name" id="name" name="name"></div>
+										<div class="6u$ 12u$(xsmall)"><input type="email" placeholder="Email" id="email" name="email"></div>
+										<div class="12u$"><textarea rows="4" placeholder="Message" id="message" name="message"></textarea></div>
+									</div>
+								</form>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message"></li>
+								</ul>
+							</div>
+							<div class="4u$ 12u$(small)">
+								<ul class="labeled-icons">
+									<li>
+										<h3 class="icon fa-home"><span class="label">Address</span></h3>
+										1234 Somewhere Rd.<br>
+										Nashville, TN 00000<br>
+										United States
+									</li>
+									<li>
+										<h3 class="icon fa-mobile"><span class="label">Phone</span></h3>
+										000-000-0000
+									</li>
+									<li>
+										<h3 class="icon fa-envelope-o"><span class="label">Email</span></h3>
+										<a href="#">hello@untitled.tld</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+    	 </div>
+    </div>
+</body>
+</html>
